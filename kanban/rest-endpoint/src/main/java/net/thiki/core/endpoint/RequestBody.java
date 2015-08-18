36510dd9873fa7eba6f58b71b74696424eaf6705
zengzhu <zengzhu@hoolai.com>
2015-08-18 18:02:02 +0800

    示例

---
+++ kanban/rest-endpoint/src/main/java/net/thiki/core/endpoint/RequestBody.java
@@ -0,0 +1,23 @@
+package net.thiki.core.endpoint;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class RequestBody extends HashMap<String, String> {
+
+    private static final long serialVersionUID = 1L;
+
+    public RequestBody(Map<String, String> map) {
+        super(map);
+    }
+    public RequestBody() {
+    }
+    
+    public int getInt(String intStr) {
+        return Integer.parseInt(intStr);
+    }
+    
+    
+
+}
