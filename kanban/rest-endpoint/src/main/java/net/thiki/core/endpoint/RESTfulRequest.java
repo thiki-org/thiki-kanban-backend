bddd370f519bdc3efef07c5ae7e33d4ea2eafa4a
fanz <fanzheng@hoolai.com>
2015-08-20 15:50:10 +0800

    add get requestParams

--- kanban/rest-endpoint/src/main/java/net/thiki/core/endpoint/RESTfulRequest.java
+++ kanban/rest-endpoint/src/main/java/net/thiki/core/endpoint/RESTfulRequest.java
@@ -2,6 +2,7 @@ package net.thiki.core.endpoint;
 
 import java.util.Map;
 
+
 /**
  * 作为HttpRequest的参数传递容器，包括?GET参数、 POST参数、 Header参数
  * @author joeaniu
@@ -9,14 +10,16 @@ import java.util.Map;
  */
 public class RESTfulRequest {
 
+	private Map<String, String[]> requestParams;
 	private Map<String, String> header;
 	private RequestBody requestBody;
 
-	public RESTfulRequest(RequestBody requestBody, Map<String, String> header) {
+	public RESTfulRequest(Map<String, String[]> requestParams, RequestBody requestBody, Map<String, String> header) {
+	    this.requestParams = requestParams;
 		this.requestBody = requestBody;
 		this.header = header;
 	}
-	
+
 	public RESTfulRequest(Map<String, String> requestBodyMap, Map<String, String> header) {
 	    if (requestBodyMap != null){
 	        this.requestBody = new RequestBody(requestBodyMap);
@@ -26,6 +29,23 @@ public class RESTfulRequest {
         this.header = header;
     }
 
+	public RESTfulRequest(Map<String, String[]> requestParams, Map<String, String> requestBodyMap, Map<String, String> header) {
+	    if (requestBodyMap != null){
+	        this.requestBody = new RequestBody(requestBodyMap);
+	    }else{
+	        this.requestBody = new RequestBody();
+	    }
+	    this.requestParams = requestParams;
+        this.header = header;
+    }
+
+	public Map<String, String[]> getRequestParams() {
+		return requestParams;
+	}
+
+	public void setRequestParams(Map<String, String[]> requestParams) {
+		this.requestParams = requestParams;
+	}
 
 	public Map<String, String> getHeader() {
 		return header;
