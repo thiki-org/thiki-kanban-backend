3e60f8d6b75584b9dedde1866cd31347eb45d715
zengzhu <zengzhu@hoolai.com>
2016-04-20 17:00:50 +0800

    send data via request body and json

--- kanban/kanban-war/src/main/java/net/thiki/controller/EntryController.java
+++ kanban/kanban-war/src/main/java/net/thiki/controller/EntryController.java
@@ -6,17 +6,16 @@ import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
 
 import net.thiki.core.endpoint.RestfulBaseService;
 import net.thiki.kanban.domain.entry.Entry;
-import net.thiki.kanban.domain.entry.EntryRepo;
 import net.thiki.service.thiki.EntryService;
 
 /**
@@ -42,10 +41,11 @@ public class EntryController extends RestfulBaseService{
     }
     
     @RequestMapping(value="entry", method = RequestMethod.POST)
-    public void add(@RequestParam(value = "title", required = true) String title, 
+    public ResponseEntity<?> add(@RequestBody Entry input, 
              @RequestHeader Map<String, String> header){
-        Entry newEntry = new Entry(title);
+        Entry newEntry = new Entry(input.getTitle());
         entryService.addEntry(newEntry);
+        return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
 }
