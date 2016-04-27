f8a1e8e7766514fe5dc7cfa82919b2a84cffd907
濤 <btao.cn@gmail.com>
2016-04-27 22:06:39 +0800

    在创建表时指定编码

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -1,6 +1,7 @@
+drop table if exists kb_entry;
 CREATE TABLE kb_entry
 (
     id INT(11) PRIMARY KEY NOT NULL auto_increment,
     title TEXT,
     deleteStatus INT(11) DEFAULT '0'
-    );
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
