0220b9f63482e07ada1f5f9448602342d16ed100
濤 <btao.cn@gmail.com>
2016-05-16 12:56:33 +0800

    add shouldDeleteSuccessfullyWhenTheEntryIsExist test.

--- src/test/java/org/thiki/kanban/entry/EntriesControllerTest.java
+++ src/test/java/org/thiki/kanban/entry/EntriesControllerTest.java
@@ -11,6 +11,7 @@ import org.thiki.kanban.TestBase;
 import static com.jayway.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Created by xubt on 5/14/16.
@@ -52,5 +53,16 @@ public class EntriesControllerTest extends TestBase {
                 .body("_links.all.href", equalTo("http://localhost:8007/entries"))
                 .body("_links.self.href", equalTo("http://localhost:8007/entries/fee"))
                 .body("_links.del.href", equalTo("http://localhost:8007/entries/fee"));
+    }
+
+    @Test
+    public void shouldDeleteSuccessfullyWhenTheEntryIsExist() {
+        jdbcTemplate.execute("INSERT INTO  kb_entry (id,title,reporter) VALUES ('fooId','this is the first entry.',1)");
+        given().header("userId", "11222")
+                .when()
+                .delete("/entries/fooId")
+                .then()
+                .statusCode(200);
+        assertEquals(1, jdbcTemplate.queryForList("select * FROM kb_entry WHERE  delete_status=1").size());
     }
 }
