3d3a76397fcc3c28d482dbdd41d402219cda2f78
濤 <btao.cn@gmail.com>
2016-07-07 21:50:17 +0800

    modifying user registration table.

--- src/main/resources/mysql_init.sql
+++ src/main/resources/mysql_init.sql
@@ -9,7 +9,7 @@ CREATE TABLE kb_user_profile
   email             VARCHAR(200) COMMENT '邮箱',
   name              VARCHAR(40),
   nick              VARCHAR(40),
-  phone            VARCHAR(40),
+  phone             VARCHAR(40),
   delete_status     INT(2)   DEFAULT 0,
   creation_time     DATETIME DEFAULT CURRENT_TIMESTAMP,
   modification_time DATETIME ON UPDATE CURRENT_TIMESTAMP,
@@ -17,19 +17,20 @@ CREATE TABLE kb_user_profile
 )
   ENGINE = InnoDB
   DEFAULT CHARSET = utf8;
-
+-- ----------------------------
+-- Table structure for kb_user_registration
+-- ----------------------------
 DROP TABLE IF EXISTS kb_user_registration;
 CREATE TABLE kb_user_registration
 (
-  id VARCHAR(40) NOT NULL,
-  user_id VARCHAR(40) NOT NULL UNIQUE COMMENT "foreign key kb_user_profile.id",
-  password varchar(63) NOT NULL,
-  recovery_email varchar(127) NOT NULL,
-  recovery_phone varchar(63) NOT NULL,
-  status int default 0,
-  delete_status int DEFAULT 0,
+  id                VARCHAR(40)  NOT NULL PRIMARY KEY,
+  password          VARCHAR(500) NOT NULL,
+  name              VARCHAR(40)  NOT NULL,
+  email             VARCHAR(127) NOT NULL,
+  salt              VARCHAR(40) DEFAULT NULL,
+  delete_status     INT         DEFAULT 0,
   creation_time     DATETIME    DEFAULT CURRENT_TIMESTAMP,
-  modification_time DATETIME DEFAULT CURRENT_TIMESTAMP,
+  modification_time DATETIME    DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (id)
 )
   ENGINE = InnoDB
@@ -107,7 +108,3 @@ CREATE TABLE kb_task_assignment (
 )
   ENGINE = InnoDB
   DEFAULT CHARSET = utf8;
-  
-ALTER TABLE kb_user_registration
-  ADD COLUMN name VARCHAR(40) DEFAULT NULL AFTER modification_time,
-  ADD COLUMN salt VARCHAR(255) DEFAULT NULL COMMENT '加密密码的盐' AFTER name;  
