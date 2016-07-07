0d4875c1e26af48111ee9a09a58ecb71ed04b0f7
濤 <btao.cn@gmail.com>
2016-07-07 21:55:13 +0800

    adding security support.

---
+++ src/main/java/org/thiki/kanban/foundation/security/md5/MD5Service.java
@@ -0,0 +1,28 @@
+package org.thiki.kanban.foundation.security.md5;
+
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * Created by xubt on 7/7/16.
+ */
+public class MD5Service {
+    public static String encrypt(String string) {
+        MessageDigest m = null;
+        String result = "";
+        try {
+            m = MessageDigest.getInstance("MD5");
+            m.update(string.getBytes("UTF8"));
+            byte s[] = m.digest();
+            for (int i = 0; i < s.length; i++) {
+                result += Integer.toHexString((0x000000ff & s[i]) | 0xffffff00).substring(6);
+            }
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException("NoSuchAlgorithmException:" + e.getMessage());
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException("UnsupportedEncodingException:" + e.getMessage());
+        }
+        return result;
+    }
+}
