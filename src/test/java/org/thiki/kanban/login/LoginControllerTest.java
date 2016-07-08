efc83d3da6252731d9df40439e786df283e7f99c
濤 <btao.cn@gmail.com>
2016-07-08 08:48:21 +0800

    adding identification test.

---
+++ src/test/java/org/thiki/kanban/login/LoginControllerTest.java
@@ -0,0 +1,28 @@
+package org.thiki.kanban.login;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.thiki.kanban.TestBase;
+import org.thiki.kanban.foundation.common.FileUtil;
+
+import static com.jayway.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by xubt on 7/5/16.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+public class LoginControllerTest extends TestBase {
+    @Test
+    public void askForAuthenticationWhenUserIsExists() {
+        String publicKey = FileUtil.readFile(publicKeyFilePath);
+        given().header("name", "foo")
+                .when()
+                .get("/identification")
+                .then()
+                .statusCode(200)
+                .body("publicKey", equalTo(publicKey));
+    }
+}
