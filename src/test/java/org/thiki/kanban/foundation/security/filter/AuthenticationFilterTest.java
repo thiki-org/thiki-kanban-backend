2f89b133047b69c6f2ff80862779ae477d550d0e
濤 <btao.cn@gmail.com>
2016-07-10 18:54:41 +0800

    renaming.

--- src/test/java/org/thiki/kanban/foundation/security/filter/AuthenticationFilterTest.java
+++ src/test/java/org/thiki/kanban/foundation/security/filter/AuthenticationFilterTest.java
@@ -1,14 +1,9 @@
 package org.thiki.kanban.foundation.security.filter;
 
-import com.jayway.restassured.RestAssured;
-import com.jayway.restassured.builder.RequestSpecBuilder;
-import com.jayway.restassured.specification.RequestSpecification;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.thiki.kanban.AuthenticationTestBase;
-import org.thiki.kanban.TestBase;
 import org.thiki.kanban.foundation.annotations.Scenario;
 
 import static com.jayway.restassured.RestAssured.given;
@@ -18,7 +13,7 @@ import static org.hamcrest.CoreMatchers.equalTo;
  * Created by xubt on 7/10/16.
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-public class StatelessAuthcFilterTest extends AuthenticationTestBase {
+public class AuthenticationFilterTest extends AuthenticationTestBase {
 
     @Scenario("当请求需要认证时,如果没有携带token,则告知客户端需要授权")
     @Test
