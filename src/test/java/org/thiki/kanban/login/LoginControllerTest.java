b995a5a764ec4e6250350e4a4bae2ab6067874fc
濤 <btao.cn@gmail.com>
2016-07-10 09:17:36 +0800

    adding scenario description.

--- src/test/java/org/thiki/kanban/login/LoginControllerTest.java
+++ src/test/java/org/thiki/kanban/login/LoginControllerTest.java
@@ -7,6 +7,7 @@ import org.junit.runner.RunWith;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.util.ReflectionTestUtils;
 import org.thiki.kanban.TestBase;
+import org.thiki.kanban.foundation.annotations.Scenario;
 import org.thiki.kanban.foundation.common.FileUtil;
 import org.thiki.kanban.foundation.security.rsa.RSAService;
 
@@ -37,6 +38,7 @@ public class LoginControllerTest extends TestBase {
         when(rsaService.encryptWithDefaultKey(any())).thenReturn("4988ca54a84321490e03fd906b5d2425afba80914c282271fd83ad1438ec8b55976cf77197a77b08c750bfb5e6173790f9f95f4e07a4f273d6fad3645e8377ed8ea865770a8aa4ff05168a98dc2417a4254405fb1639871cfc63f0dd5871a4805dc3778c106d37010b2c20adedd0117a2a8e63632744fa4e33151d880eed022e");
     }
 
+    @Scenario("当用户请求登录时,首先需要向系统发送一次认证请求,系统确认该用户合法时,将公钥发送至客户端")
     @Test
     public void askForAuthenticationWhenUserIsExists() {
         String publicKey = FileUtil.readFile(publicKeyFilePath);
@@ -48,6 +50,7 @@ public class LoginControllerTest extends TestBase {
                 .body("publicKey", equalTo(publicKey));
     }
 
+    @Scenario("用户携带通过公钥加密的密码登录系统时,系统通过私钥对其解密,解密后再通过MD5加密与数据库现有系统匹配")
     @Test
     public void loginSuccessfully() throws Exception {
         jdbcTemplate.execute("INSERT INTO  kb_user_registration (id,email,name,password,salt) " +
