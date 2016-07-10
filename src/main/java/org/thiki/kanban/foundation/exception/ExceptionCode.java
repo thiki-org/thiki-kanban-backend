b12cd16045d9189d77e46dce082d67fb75cb6a2f
濤 <btao.cn@gmail.com>
2016-07-10 17:43:52 +0800

    adding unauthorized code.

--- src/main/java/org/thiki/kanban/foundation/exception/ExceptionCode.java
+++ src/main/java/org/thiki/kanban/foundation/exception/ExceptionCode.java
@@ -4,8 +4,9 @@ public enum ExceptionCode {
 
     UNKNOWN_EX(-99),
     INVALID_PARAMS(400),
+    UNAUTHORIZED(401),
     resourceNotFound(404),
-    USER_EXISTS(100101),;
+    USER_EXISTS(100101);
 
     private int code;
 
