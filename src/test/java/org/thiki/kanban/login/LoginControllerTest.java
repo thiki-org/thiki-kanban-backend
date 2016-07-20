357c089b52c2e195a3f0bdcd33d6633f1247159e
濤 <btao.cn@gmail.com>
2016-07-20 22:53:37 +0800

    fixing test bug.

--- src/test/java/org/thiki/kanban/login/LoginControllerTest.java
+++ src/test/java/org/thiki/kanban/login/LoginControllerTest.java
@@ -29,13 +29,16 @@ public class LoginControllerTest extends TestBase {
     private RSAService rsaService;
     @Resource
     private TokenService tokenService;
+    @Resource
+    private LoginService loginService;
 
     @Before
     public void setUp() throws Exception {
         super.setUp();
         rsaService = spy(rsaService);
-        ReflectionTestUtils.setField(tokenService, "rsaService", rsaService);
         when(rsaService.encryptWithDefaultKey(any(String.class))).thenReturn("4988ca54a84321490e03fd906b5d2425afba80914c282271fd83ad1438ec8b55976cf77197a77b08c750bfb5e6173790f9f95f4e07a4f273d6fad3645e8377ed8ea865770a8aa4ff05168a98dc2417a4254405fb1639871cfc63f0dd5871a4805dc3778c106d37010b2c20adedd0117a2a8e63632744fa4e33151d880eed022e");
+        ReflectionTestUtils.setField(tokenService, "rsaService", rsaService);
+        ReflectionTestUtils.setField(loginService, "rsaService", rsaService);
     }
 
     @Scenario("用户携带通过公钥加密的密码登录系统时,系统通过私钥对其解密,解密后再通过MD5加密与数据库现有系统匹配,如果匹配通过则颁发token")
