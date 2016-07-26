2b21fdbd53126d463bdfaa7a5aa6203be7704920
濤 <btao.cn@gmail.com>
2016-07-26 21:05:20 +0800

    removing unused codes.

--- src/main/java/org/thiki/kanban/user/UserProfile.java
+++ src/main/java/org/thiki/kanban/user/UserProfile.java
@@ -5,37 +5,10 @@ import java.io.Serializable;
 public class UserProfile implements Serializable {
 
     private String id;
-
     private String email;
-
     private String name;
-    private String nick;
-    private String phone;
-
     private String creationTime;
     private String modificationTime;
-
-    /**
-     * @param id               userId
-     * @param email
-     * @param name             唯一的标示名字
-     * @param nick             用户昵称,可以重复
-     * @param phone
-     * @param creationTime
-     * @param modificationTime
-     */
-    public UserProfile(String id, String email, String name, String nick, String phone, String creationTime, String modificationTime) {
-        this.id = id;
-        this.email = email;
-        this.name = name;
-        this.nick = nick;
-        this.phone = phone;
-        this.creationTime = creationTime;
-        this.modificationTime = modificationTime;
-    }
-
-    public UserProfile() {
-    }
 
     public String getEmail() {
         return email;
@@ -76,22 +49,4 @@ public class UserProfile implements Serializable {
     public void setName(String name) {
         this.name = name;
     }
-
-    public String getNick() {
-        return nick;
-    }
-
-    public void setNick(String nick) {
-        this.nick = nick;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-
 }
