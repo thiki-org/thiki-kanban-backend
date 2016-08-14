80860bc0c6c1b17d49e2c6f4043e6d1eb01b2e80
濤 <btao.cn@gmail.com>
2016-08-14 15:14:27 +0800

    adding email format validation.

--- src/main/java/org/thiki/kanban/passwordRetrieval/PasswordRetrieval.java
+++ src/main/java/org/thiki/kanban/passwordRetrieval/PasswordRetrieval.java
@@ -1,5 +1,7 @@
 package org.thiki.kanban.passwordRetrieval;
 
+import org.hibernate.validator.constraints.Email;
+
 import javax.validation.constraints.NotNull;
 
 /**
@@ -7,6 +9,7 @@ import javax.validation.constraints.NotNull;
  */
 public class PasswordRetrieval {
     @NotNull(message = "用于找回密码的邮箱不能为空.")
+    @Email(message = "邮箱格式错误.")
     private String email;
 
     public String getEmail() {
