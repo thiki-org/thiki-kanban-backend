a88fcfe183020b2b446d9a3686459fba3d25aabd
濤 <btao.cn@gmail.com>
2016-08-14 23:07:32 +0800

    Finishing createRetrievalRecord method.

--- src/main/java/org/thiki/kanban/passwordRetrieval/PasswordRetrievalService.java
+++ src/main/java/org/thiki/kanban/passwordRetrieval/PasswordRetrievalService.java
@@ -1,25 +1,51 @@
 package org.thiki.kanban.passwordRetrieval;
 
+import freemarker.template.TemplateException;
 import org.springframework.stereotype.Service;
+import org.thiki.kanban.foundation.common.VerificationCodeService;
 import org.thiki.kanban.foundation.exception.BusinessException;
-import org.thiki.kanban.team.Team;
+import org.thiki.kanban.foundation.mail.MailService;
+import org.thiki.kanban.registration.Registration;
+import org.thiki.kanban.registration.RegistrationPersistence;
 
 import javax.annotation.Resource;
+import javax.mail.MessagingException;
+import java.io.IOException;
 
 /**
  * Created by xubt on 8/8/16.
  */
 @Service
 public class PasswordRetrievalService {
+
+    private static final String passwordRetrievalEmailTemplate = "passwordRetrieval.ftl";
     @Resource
     private PasswordRetrievalPersistence passwordRetrievalPersistence;
+    @Resource
+    private VerificationCodeService verificationCodeService;
+    @Resource
+    private RegistrationPersistence registrationPersistence;
 
-    public Team createRetrievalRecord(RegisterEmail registerEmail) {
+    @Resource
+    private MailService mailService;
 
-        boolean isFoundEmail = passwordRetrievalPersistence.existsEmail(registerEmail.getEmail());
-        if (!isFoundEmail) {
+    public void createRetrievalRecord(RegisterEmail registerEmail) throws TemplateException, IOException, MessagingException {
+        Registration registeredUser = registrationPersistence.findByEmail(registerEmail.getEmail());
+        if (registeredUser == null) {
             throw new BusinessException(PasswordRetrievalCodes.EmailIsNotExists.code(), PasswordRetrievalCodes.EmailIsNotExists.message());
         }
-        return null;
+
+        String verificationCode = verificationCodeService.generate();
+
+        PasswordRetrieval passwordRetrieval = new PasswordRetrieval();
+        passwordRetrieval.setEmail(registerEmail.getEmail());
+        passwordRetrieval.setVerificationCode(verificationCode);
+        passwordRetrievalPersistence.createRecord(passwordRetrieval);
+
+        PasswordEmail passwordEmail = new PasswordEmail();
+        passwordEmail.setReceiver(registeredUser.getEmail());
+        passwordEmail.setUserName(registeredUser.getName());
+        passwordEmail.setVerificationCode(verificationCode);
+        mailService.sendMailByTemplate(passwordEmail, passwordRetrievalEmailTemplate);
     }
 }
