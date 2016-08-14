8d9b9e87f21cbd32879305e98d2c07057c12e083
濤 <btao.cn@gmail.com>
2016-08-14 15:38:19 +0800

    adding email is not exists validation.

--- src/main/java/org/thiki/kanban/passwordRetrieval/PasswordRetrievalService.java
+++ src/main/java/org/thiki/kanban/passwordRetrieval/PasswordRetrievalService.java
@@ -1,14 +1,25 @@
 package org.thiki.kanban.passwordRetrieval;
 
 import org.springframework.stereotype.Service;
+import org.thiki.kanban.foundation.exception.BusinessException;
 import org.thiki.kanban.team.Team;
+
+import javax.annotation.Resource;
 
 /**
  * Created by xubt on 8/8/16.
  */
 @Service
 public class PasswordRetrievalService {
+    @Resource
+    private PasswordRetrievalPersistence passwordRetrievalPersistence;
+
     public Team createRetrievalRecord(PasswordRetrieval passwordRetrieval) {
+
+        boolean isFoundEmail = passwordRetrievalPersistence.existsEmail(passwordRetrieval.getEmail());
+        if (!isFoundEmail) {
+            throw new BusinessException(PasswordRetrievalCodes.EmailIsNotExists.code(), PasswordRetrievalCodes.EmailIsNotExists.message());
+        }
         return null;
     }
 }
--- src/test/java/org/thiki/kanban/passwordRetrieval/PasswordRetrievalControllerTest.java
+++ src/test/java/org/thiki/kanban/passwordRetrieval/PasswordRetrievalControllerTest.java
@@ -3,6 +3,7 @@ package org.thiki.kanban.passwordRetrieval;
 import com.jayway.restassured.http.ContentType;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.http.HttpStatus;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.thiki.kanban.TestBase;
 import org.thiki.kanban.foundation.annotations.Scenario;
@@ -20,14 +21,39 @@ public class PasswordRetrievalControllerTest extends TestBase {
     @Scenario("当用户请求找回密码时,需要提供邮箱,如果未提供则告知客户端错误")
     @Test
     public void NotAllowedIfEmailIsNotProvide() {
-        given().header("userId", "11222")
-                .body("{\"noEmail\":\"noEmail\"}")
+        given().body("{\"noEmail\":\"noEmail\"}")
                 .contentType(ContentType.JSON)
                 .when()
                 .post("/passwordRetrieval")
                 .then()
-                .statusCode(ExceptionCode.INVALID_PARAMS.code())
+                .statusCode(HttpStatus.BAD_REQUEST.value())
                 .body("code", equalTo(ExceptionCode.INVALID_PARAMS.code()))
                 .body("message", equalTo("用于找回密码的邮箱不能为空."));
+    }
+
+    @Scenario("当用户请求找回密码时,需要提供邮箱,如果邮箱格式错误则告知客户端错误")
+    @Test
+    public void NotAllowedIfEmailFormatIsNotCorrect() {
+        given().body("{\"email\":\"email\"}")
+                .contentType(ContentType.JSON)
+                .when()
+                .post("/passwordRetrieval")
+                .then()
+                .statusCode(HttpStatus.BAD_REQUEST.value())
+                .body("code", equalTo(ExceptionCode.INVALID_PARAMS.code()))
+                .body("message", equalTo("邮箱格式错误."));
+    }
+
+    @Scenario("当用户请求找回密码时,需要提供邮箱,如果邮箱不存在则告知客户端错误")
+    @Test
+    public void NotAllowedIfEmailFormatIsNotExists() {
+        given().body("{\"email\":\"email@email.com\"}")
+                .contentType(ContentType.JSON)
+                .when()
+                .post("/passwordRetrieval")
+                .then()
+                .statusCode(HttpStatus.BAD_REQUEST.value())
+                .body("code", equalTo(PasswordRetrievalCodes.EmailIsNotExists.code()))
+                .body("message", equalTo(PasswordRetrievalCodes.EmailIsNotExists.message()));
     }
 }
