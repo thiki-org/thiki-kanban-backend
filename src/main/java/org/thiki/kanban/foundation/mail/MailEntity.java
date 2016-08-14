1b57d639d9ce9768679bcd1bc828a43816fb4fd9
濤 <btao.cn@gmail.com>
2016-08-14 23:00:46 +0800

    Adding mailEntry.

---
+++ src/main/java/org/thiki/kanban/foundation/mail/MailEntity.java
@@ -0,0 +1,45 @@
+package org.thiki.kanban.foundation.mail;
+
+/**
+ * Created by xubt on 8/14/16.
+ */
+public class MailEntity {
+    private String userName;
+
+    private String receiver;
+    private String subject;
+
+    private String dateline;
+
+    public String getReceiver() {
+        return receiver;
+    }
+
+    public void setReceiver(String receiver) {
+        this.receiver = receiver;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getDateline() {
+        return dateline;
+    }
+
+    public void setDateline(String dateline) {
+        this.dateline = dateline;
+    }
+}
