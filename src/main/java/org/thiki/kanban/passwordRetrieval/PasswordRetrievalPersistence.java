566509b831aac6f7995bd74be8ccc049b3f61fc0
濤 <btao.cn@gmail.com>
2016-08-14 23:06:58 +0800

    Removing existsEmail and Adding createRecord method.

--- src/main/java/org/thiki/kanban/passwordRetrieval/PasswordRetrievalPersistence.java
+++ src/main/java/org/thiki/kanban/passwordRetrieval/PasswordRetrievalPersistence.java
@@ -8,5 +8,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PasswordRetrievalPersistence {
-    Boolean existsEmail(String email);
+    Integer createRecord(PasswordRetrieval passwordRetrieval);
 }
