86c0e9c974615da4e65ddaf80e57def227b7b4ea
濤 <btao.cn@gmail.com>
2016-08-15 20:49:03 +0800

    Adding password entity.

---
+++ src/main/java/org/thiki/kanban/passwordRetrieval/Password.java
@@ -0,0 +1,25 @@
+package org.thiki.kanban.passwordRetrieval;
+
+/**
+ * Created by xubt on 8/15/16.
+ */
+public class Password {
+    private String email;
+    private String password;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
