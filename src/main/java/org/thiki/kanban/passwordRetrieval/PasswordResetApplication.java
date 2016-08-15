cb1de1f104d5328b5d1a3b7ebc1fbc7aead54efb
濤 <btao.cn@gmail.com>
2016-08-15 13:41:19 +0800

    Adding PasswordResetApplication.

---
+++ src/main/java/org/thiki/kanban/passwordRetrieval/PasswordResetApplication.java
@@ -0,0 +1,34 @@
+package org.thiki.kanban.passwordRetrieval;
+
+/**
+ * Created by xubt on 8/15/16.
+ */
+public class PasswordResetApplication {
+    private String id;
+
+    private String email;
+    private String verificationCode;
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getVerificationCode() {
+        return verificationCode;
+    }
+
+    public void setVerificationCode(String verificationCode) {
+        this.verificationCode = verificationCode;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
