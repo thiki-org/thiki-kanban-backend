416f1cc8e191a680759ed762ceaaed1f73a94da1
濤 <btao.cn@gmail.com>
2016-08-15 13:40:17 +0800

    Adding PasswordRestResource.

---
+++ src/main/java/org/thiki/kanban/passwordRetrieval/PasswordResetResource.java
@@ -0,0 +1,17 @@
+package org.thiki.kanban.passwordRetrieval;
+
+import org.springframework.hateoas.Link;
+import org.thiki.kanban.foundation.common.RestResource;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
+
+/**
+ * Created by xubt on 8/15/16.
+ */
+public class PasswordResetResource extends RestResource {
+    public PasswordResetResource() throws Exception {
+        Link passwordRetrievalLink = linkTo(methodOn(PasswordRetrievalController.class).password(null)).withRel("password");
+        this.add(passwordRetrievalLink);
+    }
+}
