f69611c6e87ec4accf68398724581ff7de9035ac
濤 <btao.cn@gmail.com>
2016-08-16 08:52:37 +0800

    Adding SECURITY_CODE_TIMEOUT enum.

--- src/main/java/org/thiki/kanban/passwordRetrieval/PasswordRetrievalCodes.java
+++ src/main/java/org/thiki/kanban/passwordRetrieval/PasswordRetrievalCodes.java
@@ -4,7 +4,8 @@ package org.thiki.kanban.passwordRetrieval;
  * Created by xubt on 8/8/16.
  */
 public enum PasswordRetrievalCodes {
-    EMAIL_IS_NOT_EXISTS(20011, "邮箱不存在。");
+    EMAIL_IS_NOT_EXISTS(20011, "邮箱不存在。"),
+    SECURITY_CODE_TIMEOUT(20012, "验证码已过期。");
     private int code;
     private String message;
 
