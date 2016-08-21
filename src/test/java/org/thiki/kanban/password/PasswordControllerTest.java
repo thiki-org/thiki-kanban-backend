4fd84a00cfbbee70cb7d588f259c65e00e798e02
濤 <btao.cn@gmail.com>
2016-08-21 12:05:59 +0800

    Optimizing tests logic.

--- src/test/java/org/thiki/kanban/password/PasswordControllerTest.java
+++ src/test/java/org/thiki/kanban/password/PasswordControllerTest.java
@@ -82,97 +82,96 @@ public class PasswordControllerTest extends TestBase {
     @Test
     public void sendVerificationCode() {
         jdbcTemplate.execute("INSERT INTO  kb_user_registration (id,email,name,password) " +
-                "VALUES ('fooUserId','766191920@qq.com','徐涛','password')");
+                "VALUES ('fooUserId','766191920@qq.com','tao','password')");
         given().body("{\"email\":\"766191920@qq.com\"}")
                 .contentType(ContentType.JSON)
                 .when()
                 .post("/passwordRetrievalApplication")
                 .then()
                 .statusCode(HttpStatus.CREATED.value())
-                .body("_links.passwordResetApplication.href", equalTo("http://localhost:8007/passwordResetApplication"));
-        assertEquals(1, jdbcTemplate.queryForList("SELECT * FROM kb_password_retrieval where email='766191920@qq.com'").size());
+                .body("_links.passwordResetApplication.href", equalTo("http://localhost:8007/tao/passwordResetApplication"));
+        assertEquals(1, jdbcTemplate.queryForList("SELECT * FROM kb_password_retrieval where user_name='tao'").size());
     }
 
     @Scenario("邮箱通过格式校验且存在后，创建密码找回申请记前,如果存在未完成的申请,则将其废弃")
     @Test
     public void discardingUnfinishedPasswordRetrievalApplication() {
         jdbcTemplate.execute("INSERT INTO  kb_user_registration (id,email,name,password) " +
-                "VALUES ('fooUserId','766191920@qq.com','徐涛','password')");
-        jdbcTemplate.execute("INSERT INTO  kb_password_retrieval (id,email,verification_code) " +
-                "VALUES ('fooUserId','766191920@qq.com','000000')");
+                "VALUES ('fooUserId','766191920@qq.com','tao','password')");
+        jdbcTemplate.execute("INSERT INTO  kb_password_retrieval (id,user_name,verification_code) " +
+                "VALUES ('fooUserId','tao','000000')");
         given().body("{\"email\":\"766191920@qq.com\"}")
                 .contentType(ContentType.JSON)
                 .when()
                 .post("/passwordRetrievalApplication")
                 .then()
                 .statusCode(HttpStatus.CREATED.value())
-                .body("_links.passwordResetApplication.href", equalTo("http://localhost:8007/passwordResetApplication"));
-        assertEquals(1, jdbcTemplate.queryForList("SELECT * FROM kb_password_retrieval where email='766191920@qq.com' and is_verify_passed=0").size());
-        assertEquals(1, jdbcTemplate.queryForList("SELECT * FROM kb_password_retrieval where email='766191920@qq.com' and is_verify_passed=-1").size());
+                .body("_links.passwordResetApplication.href", equalTo("http://localhost:8007/tao/passwordResetApplication"));
+        assertEquals(1, jdbcTemplate.queryForList("SELECT * FROM kb_password_retrieval where user_name='tao' and is_verify_passed=0").size());
+        assertEquals(1, jdbcTemplate.queryForList("SELECT * FROM kb_password_retrieval where user_name='tao' and is_verify_passed=-1").size());
     }
 
     @Scenario("用户取得验证码后，和邮箱一起发送到服务端验证，如果验证码正确且未过期，则发送密码重置的链接")
     @Test
     public void verifyVerificationCode() {
         jdbcTemplate.execute("INSERT INTO  kb_user_registration (id,email,name,password) " +
-                "VALUES ('fooUserId','766191920@qq.com','徐涛','password')");
-        jdbcTemplate.execute("INSERT INTO  kb_password_retrieval (id,email,verification_code) " +
-                "VALUES ('fooUserId','766191920@qq.com','000000')");
+                "VALUES ('fooUserId','766191920@qq.com','tao','password')");
+        jdbcTemplate.execute("INSERT INTO  kb_password_retrieval (id,user_name,verification_code) " +
+                "VALUES ('fooUserId','tao','000000')");
 
         VerificationCodeService verificationCodeService = mock(VerificationCodeService.class);
         when(verificationCodeService.generate()).thenReturn("000000");
         ReflectionTestUtils.setField(passwordService, "verificationCodeService", verificationCodeService);
 
-        given().body("{\"email\":\"766191920@qq.com\",\"verificationCode\":\"000000\"}")
+        given().body("{\"verificationCode\":\"000000\"}")
                 .contentType(ContentType.JSON)
                 .when()
-                .post("/passwordResetApplication")
+                .post("/tao/passwordResetApplication")
                 .then()
                 .statusCode(HttpStatus.CREATED.value())
-                .body("_links.password.href", equalTo("http://localhost:8007/password"));
-        assertEquals(1, jdbcTemplate.queryForList("SELECT * FROM kb_password_retrieval where email='766191920@qq.com' and is_verify_passed=1").size());
-        assertEquals(1, jdbcTemplate.queryForList("SELECT * FROM kb_password_reset where email='766191920@qq.com'").size());
+                .body("_links.password.href", equalTo("http://localhost:8007/tao/password"));
+        assertEquals(1, jdbcTemplate.queryForList("SELECT * FROM kb_password_retrieval where user_name='tao' and is_verify_passed=1").size());
+        assertEquals(1, jdbcTemplate.queryForList("SELECT * FROM kb_password_reset where user_name='tao'").size());
     }
 
     @Scenario("用户通过验证码验证,重置密码成功。")
     @Test
     public void resetPassword() throws Exception {
         jdbcTemplate.execute("INSERT INTO  kb_user_registration (id,email,name,password) " +
-                "VALUES ('fooUserId','766191920@qq.com','徐涛','password')");
-        jdbcTemplate.execute("INSERT INTO  kb_password_reset(id,email) " +
-                "VALUES ('fooUserId','766191920@qq.com')");
+                "VALUES ('fooUserId','766191920@qq.com','tao','password')");
+        jdbcTemplate.execute("INSERT INTO  kb_password_reset(id,user_name) " +
+                "VALUES ('fooUserId','tao')");
         String publicKey = rsaService.loadKey(publicKeyFilePath);
         String password = "foo";
         String rsaPassword = rsaService.encrypt(publicKey, password);
-        String expectedMd5Password = "979e14ce78f745bbd78bc4e7533d600e";
+        String expectedMd5Password = "acbd18db4cc2f85cedef654fccc4a4d8";
 
         VerificationCodeService verificationCodeService = mock(VerificationCodeService.class);
         when(verificationCodeService.generate()).thenReturn("000000");
         ReflectionTestUtils.setField(passwordService, "verificationCodeService", verificationCodeService);
 
         JSONObject body = new JSONObject();
-        body.put("email", "766191920@qq.com");
         body.put("password", rsaPassword);
         given().body(body)
                 .contentType(ContentType.JSON)
                 .when()
-                .put("/password")
+                .put("/tao/password")
                 .then()
                 .statusCode(HttpStatus.CREATED.value())
                 .body("_links.login.href", equalTo("http://localhost:8007/login"));
-        assertEquals(expectedMd5Password, jdbcTemplate.queryForObject("SELECT password FROM kb_user_registration where email='766191920@qq.com'", String.class));
-        assertEquals(1, jdbcTemplate.queryForList("SELECT * FROM kb_password_reset where email='766191920@qq.com' and is_reset=1").size());
+        assertEquals(expectedMd5Password, jdbcTemplate.queryForObject("SELECT password FROM kb_user_registration where name='tao'", String.class));
+        assertEquals(1, jdbcTemplate.queryForList("SELECT * FROM kb_password_reset where user_name='tao' and is_reset=1").size());
     }
 
     @Scenario("验证码超过五分钟后,验证失败")
     @Test
     public void verificationCodeTimeOut() throws Exception {
         jdbcTemplate.execute("INSERT INTO  kb_user_registration (id,email,name,password) " +
-                "VALUES ('fooUserId','766191920@qq.com','徐涛','password')");
+                "VALUES ('fooUserId','766191920@qq.com','tao','password')");
 
         String dateFiveMinutesAgo = dateService.DateToString(dateService.addMinute(new Date(), -6), DateStyle.YYYY_MM_DD_HH_MM_SS);
-        jdbcTemplate.execute(String.format("INSERT INTO  kb_password_retrieval (id,email,verification_code,creation_time) " +
-                "VALUES ('fooUserId','766191920@qq.com','000000','%s')", dateFiveMinutesAgo));
+        jdbcTemplate.execute(String.format("INSERT INTO  kb_password_retrieval (id,user_name,verification_code,creation_time) " +
+                "VALUES ('fooUserId','tao','000000','%s')", dateFiveMinutesAgo));
 
         VerificationCodeService verificationCodeService = mock(VerificationCodeService.class);
         when(verificationCodeService.generate()).thenReturn("000000");
@@ -181,19 +180,19 @@ public class PasswordControllerTest extends TestBase {
         given().body("{\"email\":\"766191920@qq.com\",\"verificationCode\":\"000000\"}")
                 .contentType(ContentType.JSON)
                 .when()
-                .post("/passwordResetApplication")
+                .post("/tao/passwordResetApplication")
                 .then()
                 .statusCode(HttpStatus.BAD_REQUEST.value())
                 .body("code", equalTo(PasswordCodes.SECURITY_CODE_TIMEOUT.code()))
                 .body("message", equalTo(PasswordCodes.SECURITY_CODE_TIMEOUT.message()));
-        assertEquals(1, jdbcTemplate.queryForList("SELECT * FROM kb_password_retrieval where email='766191920@qq.com'").size());
+        assertEquals(1, jdbcTemplate.queryForList("SELECT * FROM kb_password_retrieval where user_name='tao'").size());
     }
 
     @Scenario("验证码错误,验证失败")
     @Test
     public void VerificationWillBeFailedIfVerificationCodeIsNotCorrect() throws Exception {
-        jdbcTemplate.execute("INSERT INTO  kb_password_retrieval (id,email,verification_code) " +
-                "VALUES ('fooUserId','766191920@qq.com','000000')");
+        jdbcTemplate.execute("INSERT INTO  kb_password_retrieval (id,user_name,verification_code) " +
+                "VALUES ('fooUserId','tao','000000')");
 
         VerificationCodeService verificationCodeService = mock(VerificationCodeService.class);
         when(verificationCodeService.generate()).thenReturn("000000");
@@ -202,7 +201,7 @@ public class PasswordControllerTest extends TestBase {
         given().body("{\"email\":\"766191920@qq.com\",\"verificationCode\":\"000001\"}")
                 .contentType(ContentType.JSON)
                 .when()
-                .post("/passwordResetApplication")
+                .post("/tao/passwordResetApplication")
                 .then()
                 .statusCode(HttpStatus.BAD_REQUEST.value())
                 .body("code", equalTo(PasswordCodes.SECURITY_CODE_IS_NOT_CORRECT.code()))
@@ -215,27 +214,27 @@ public class PasswordControllerTest extends TestBase {
         given().body("{\"email\":\"766191920@qq.com\",\"verificationCode\":\"000000\"}")
                 .contentType(ContentType.JSON)
                 .when()
-                .post("/passwordResetApplication")
+                .post("/tao/passwordResetApplication")
                 .then()
                 .statusCode(HttpStatus.BAD_REQUEST.value())
                 .body("code", equalTo(PasswordCodes.NO_PASSWORD_RETRIEVAL_RECORD.code()))
                 .body("message", equalTo(PasswordCodes.NO_PASSWORD_RETRIEVAL_RECORD.message()));
-        assertEquals(0, jdbcTemplate.queryForList("SELECT * FROM kb_password_reset where email='766191920@qq.com'").size());
+        assertEquals(0, jdbcTemplate.queryForList("SELECT * FROM kb_password_reset where user_name='tao'").size());
     }
 
     @Scenario("用户重置密码后，若再次重置，告知客户端请求无效")
     @Test
     public void ResetPasswordIsNotAllowedIfTheApplicationHasBeenAlreadyReset() throws Exception {
-        jdbcTemplate.execute("INSERT INTO  kb_password_reset(id,email,is_reset) " +
-                "VALUES ('fooUserId','766191920@qq.com',1)");
+        jdbcTemplate.execute("INSERT INTO  kb_password_reset(id,user_name,is_reset) " +
+                "VALUES ('fooUserId','tao',1)");
         JSONObject body = new JSONObject();
-        body.put("email", "766191920@qq.com");
         body.put("password", "foo");
         given().body(body)
                 .contentType(ContentType.JSON)
                 .when()
-                .put("/password")
+                .put("/tao/password")
                 .then()
+                .statusCode(HttpStatus.BAD_REQUEST.value())
                 .body("code", equalTo(PasswordCodes.NO_PASSWORD_RESET_RECORD.code()))
                 .body("message", equalTo(PasswordCodes.NO_PASSWORD_RESET_RECORD.message()));
     }
