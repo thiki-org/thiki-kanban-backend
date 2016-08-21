e3785da3f415ed52b2e865b2200a72e608c19934
濤 <btao.cn@gmail.com>
2016-08-21 12:00:43 +0800

    Removing email field and adding userName field.

--- src/main/java/org/thiki/kanban/password/passwordReset/PasswordResetApplication.java
+++ src/main/java/org/thiki/kanban/password/passwordReset/PasswordResetApplication.java
@@ -1,6 +1,5 @@
 package org.thiki.kanban.password.passwordReset;
 
-import org.hibernate.validator.constraints.Email;
 import org.hibernate.validator.constraints.Length;
 
 import javax.validation.constraints.NotNull;
@@ -10,21 +9,18 @@ import javax.validation.constraints.NotNull;
  */
 public class PasswordResetApplication {
     private String id;
-    @NotNull(message = "邮箱不能为空。")
-    @Email(message = "邮箱格式不正确。")
-    @Length(max = 40, message = "邮箱超出长度限制。")
-    private String email;
+    private String userName;
 
     @NotNull(message = "验证码不能为空。")
     @Length(min = 6, max = 6, message = "验证码格式错误,应为6为数字。")
     private String verificationCode;
 
-    public String getEmail() {
-        return email;
+    public String getUserName() {
+        return userName;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
+    public void setUserName(String userName) {
+        this.userName = userName;
     }
 
     public String getVerificationCode() {
