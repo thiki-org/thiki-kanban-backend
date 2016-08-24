2d5d93642b11227878e200a4db699f361c183bb9
濤 <btao.cn@gmail.com>
2016-08-24 10:37:47 +0800

    Adding constructor.

--- src/main/java/org/thiki/kanban/foundation/exception/InvalidParamsException.java
+++ src/main/java/org/thiki/kanban/foundation/exception/InvalidParamsException.java
@@ -7,12 +7,15 @@ import org.springframework.web.bind.annotation.ResponseStatus;
  * Created by xubt on 8/24/16.
  */
 @ResponseStatus(value = HttpStatus.BAD_REQUEST)
-
 public class InvalidParamsException extends BusinessException {
 
     private static final long serialVersionUID = -5266231525138811045L;
 
     public InvalidParamsException(int errorCode, String message) {
         super(errorCode, message, HttpStatus.BAD_REQUEST);
+    }
+
+    public InvalidParamsException(String message) {
+        super(HttpStatus.BAD_REQUEST.value(), message, HttpStatus.BAD_REQUEST);
     }
 }
