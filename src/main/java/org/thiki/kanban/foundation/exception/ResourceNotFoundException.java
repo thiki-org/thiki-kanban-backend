f7301824c90e29a3b375d4832e0da9241abac751
濤 <btao.cn@gmail.com>
2016-08-24 10:39:08 +0800

    Adding HttpStatus annotation.

--- src/main/java/org/thiki/kanban/foundation/exception/ResourceNotFoundException.java
+++ src/main/java/org/thiki/kanban/foundation/exception/ResourceNotFoundException.java
@@ -1,7 +1,9 @@
 package org.thiki.kanban.foundation.exception;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class ResourceNotFoundException extends BusinessException {
 
     private static final long serialVersionUID = -5266231525138811045L;
