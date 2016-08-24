e78a054115ee89865e26a5783d03a493105b76e9
濤 <btao.cn@gmail.com>
2016-08-24 08:53:20 +0800

    Adding cardsCodes.

---
+++ src/main/java/org/thiki/kanban/card/CardsCodes.java
@@ -0,0 +1,23 @@
+package org.thiki.kanban.card;
+
+/**
+ * Created by xubt on 8/8/16.
+ */
+public enum CardsCodes {
+    CARD_IS_NOT_EXISTS(40011, "卡片未找到。");
+    private int code;
+    private String message;
+
+    CardsCodes(int code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    public int code() {
+        return code;
+    }
+
+    public String message() {
+        return message;
+    }
+}
