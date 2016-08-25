59cc3f65edb32d8b134873ca8c46472f97b62a30
濤 <btao.cn@gmail.com>
2016-08-25 16:08:00 +0800

    Adding boardCodes.

---
+++ src/main/java/org/thiki/kanban/board/BoardCodes.java
@@ -0,0 +1,25 @@
+package org.thiki.kanban.board;
+
+/**
+ * Created by xubt on 8/8/16.
+ */
+public enum BoardCodes {
+    BOARD_IS_ALREADY_EXISTS(30011, "同名看板已经存在,请使用其它名称。");
+    private int code;
+    private String message;
+    public static final String nameIsRequired = "看板名称不能为空。";
+    public static final String nameIsInvalid = "看板名称过长,请保持在10个字以内。";
+
+    BoardCodes(int code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    public int code() {
+        return code;
+    }
+
+    public String message() {
+        return message;
+    }
+}
