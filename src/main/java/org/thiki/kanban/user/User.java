5a1f4c13acf31bb460e33cee499e3c083f4dd16b
濤 <btao.cn@gmail.com>
2016-09-14 08:56:01 +0800

    Adding User.

---
+++ src/main/java/org/thiki/kanban/user/User.java
@@ -0,0 +1,54 @@
+package org.thiki.kanban.user;
+
+/**
+ * Created by xubt on 7/7/16.
+ */
+public class User {
+
+    private String id;
+    private String email;
+    private String name;
+    private String password;
+
+    private String salt;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getSalt() {
+        return salt;
+    }
+
+    public void setSalt(String salt) {
+        this.salt = salt;
+    }
+}
