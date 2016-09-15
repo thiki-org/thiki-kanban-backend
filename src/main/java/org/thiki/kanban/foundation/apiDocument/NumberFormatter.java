2c88e9c7ea9749e57550a6833669716f76161d5a
濤 <btao.cn@gmail.com>
2016-09-15 12:21:36 +0800

    Fixing convert bug.

--- src/main/java/org/thiki/kanban/foundation/apiDocument/NumberFormatter.java
+++ src/main/java/org/thiki/kanban/foundation/apiDocument/NumberFormatter.java
@@ -1,5 +1,7 @@
 package org.thiki.kanban.foundation.apiDocument;
 
+import java.math.BigDecimal;
+
 /**
  * Created by xubt on 9/15/16.
  */
@@ -14,27 +16,9 @@ public class NumberFormatter {
         if (chineseStringArray.length != 2 || !isNumeric(chineseStringArray[0])) {
             return chineseString;
         }
-
-        int num = Integer.parseInt(chineseStringArray[0]);
-        char[] val = String.valueOf(num).toCharArray();
-        int len = val.length;
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < len; i++) {
-            String m = val[i] + "";
-            int n = Integer.valueOf(m);
-            boolean isZero = n == 0;
-            String unit = units[(len - 1) - i];
-            if (isZero) {
-                if ('0' == val[i - 1]) {
-                } else {
-                    sb.append(numArray[n]);
-                }
-            } else {
-                sb.append(numArray[n]);
-                sb.append(unit);
-            }
-        }
-        return sb.toString() + "、" + chineseStringArray[1];
+        double money = Integer.parseInt(chineseStringArray[0]);
+        BigDecimal numberOfMoney = new BigDecimal(money);
+        return number2CNMontrayUnit(numberOfMoney) + "、" + chineseStringArray[1];
     }
 
     private static boolean isNumeric(String str) {
@@ -45,5 +29,114 @@ public class NumberFormatter {
             }
         }
         return true;
+    }
+
+    /**
+     * 汉语中数字大写
+     */
+    private static final String[] CN_UPPER_NUMBER = {"零", "一", "二", "三", "四",
+            "五", "六", "七", "八", "九"};
+    /**
+     * 汉语中货币单位大写，这样的设计类似于占位符
+     */
+    private static final String[] CN_UPPER_MONETRAY_UNIT = {"", "", "",
+            "", "", "", "", "拾", "佰", "仟", "亿", "拾", "佰", "仟", "兆", "拾",
+            "佰", "仟"};
+    /**
+     * 特殊字符：整
+     */
+    private static final String CN_FULL = "";
+    /**
+     * 特殊字符：负
+     */
+    private static final String CN_NEGATIVE = "负";
+    /**
+     * 金额的精度，默认值为2
+     */
+    private static final int MONEY_PRECISION = 2;
+    /**
+     * 特殊字符：零元整
+     */
+    private static final String CN_ZEOR_FULL = "零" + CN_FULL;
+
+    /**
+     * 把输入的金额转换为汉语中人民币的大写
+     *
+     * @param numberOfMoney 输入的金额
+     * @return 对应的汉语大写
+     */
+    public static String number2CNMontrayUnit(BigDecimal numberOfMoney) {
+        StringBuffer sb = new StringBuffer();
+        // -1, 0, or 1 as the value of this BigDecimal is negative, zero, or
+        // positive.
+        int signum = numberOfMoney.signum();
+        // 零元整的情况
+        if (signum == 0) {
+            return CN_ZEOR_FULL;
+        }
+        //这里会进行金额的四舍五入
+        long number = numberOfMoney.movePointRight(MONEY_PRECISION)
+                .setScale(0, 4).abs().longValue();
+        // 得到小数点后两位值
+        long scale = number % 100;
+        int numUnit = 0;
+        int numIndex = 0;
+        boolean getZero = false;
+        // 判断最后两位数，一共有四中情况：00 = 0, 01 = 1, 10, 11
+        if (!(scale > 0)) {
+            numIndex = 2;
+            number = number / 100;
+            getZero = true;
+        }
+        if ((scale > 0) && (!(scale % 10 > 0))) {
+            numIndex = 1;
+            number = number / 10;
+            getZero = true;
+        }
+        int zeroSize = 0;
+        while (true) {
+            if (number <= 0) {
+                break;
+            }
+            // 每次获取到最后一个数
+            numUnit = (int) (number % 10);
+            if (numUnit > 0) {
+                if ((numIndex == 9) && (zeroSize >= 3)) {
+                    sb.insert(0, CN_UPPER_MONETRAY_UNIT[6]);
+                }
+                if ((numIndex == 13) && (zeroSize >= 3)) {
+                    sb.insert(0, CN_UPPER_MONETRAY_UNIT[10]);
+                }
+                sb.insert(0, CN_UPPER_MONETRAY_UNIT[numIndex]);
+                sb.insert(0, CN_UPPER_NUMBER[numUnit]);
+                getZero = false;
+                zeroSize = 0;
+            } else {
+                ++zeroSize;
+                if (!(getZero)) {
+                    sb.insert(0, CN_UPPER_NUMBER[numUnit]);
+                }
+                if (numIndex == 2) {
+                    if (number > 0) {
+                        sb.insert(0, CN_UPPER_MONETRAY_UNIT[numIndex]);
+                    }
+                } else if (((numIndex - 2) % 4 == 0) && (number % 1000 > 0)) {
+                    sb.insert(0, CN_UPPER_MONETRAY_UNIT[numIndex]);
+                }
+                getZero = true;
+            }
+            // 让number每次都去掉最后一个数
+            number = number / 10;
+            ++numIndex;
+        }
+        // 如果signum == -1，则说明输入的数字为负数，就在最前面追加特殊字符：负
+        if (signum == -1) {
+            sb.insert(0, CN_NEGATIVE);
+        }
+        // 输入的数字小数点后两位为"00"的情况，则要在最后追加特殊字符：整
+        if (!(scale > 0)) {
+            sb.append(CN_FULL);
+        }
+        return sb.toString();
     }
 }
