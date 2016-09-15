fcba730751dff09b73760f6971f47d7436bede60
濤 <btao.cn@gmail.com>
2016-09-15 13:16:10 +0800

    Introducing DomainOrder.

---
+++ src/main/java/org/thiki/kanban/foundation/application/DomainOrder.java
@@ -0,0 +1,18 @@
+package org.thiki.kanban.foundation.application;
+
+/**
+ * Created by xubt on 9/15/16.
+ */
+public class DomainOrder {
+
+    public static final int ENTRANCE = 0;
+    public static final int REGISTRATION = 1;
+    public static final int LOGIN = 2;
+    public static final int BOARD = 3;
+    public static final int PROCEDURE = 4;
+    public static final int CARD = 5;
+    public static final int TEAM = 6;
+    public static final int INVITATION = 7;
+    public static final int TEAM_MEMBER = 8;
+    public static final int PASSWORD = 9;
+}
