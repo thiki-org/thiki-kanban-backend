d309a2c95df44eced7051057d811beb3c877ea0f
濤 <btao.cn@gmail.com>
2016-09-20 09:23:16 +0800

    Adding notifications rel.

--- src/main/java/org/thiki/kanban/login/IdentificationResource.java
+++ src/main/java/org/thiki/kanban/login/IdentificationResource.java
@@ -1,13 +1,10 @@
 package org.thiki.kanban.login;
 
-import freemarker.template.TemplateException;
 import org.springframework.hateoas.Link;
 import org.thiki.kanban.board.BoardsController;
 import org.thiki.kanban.foundation.common.RestResource;
+import org.thiki.kanban.notification.NotificationController;
 import org.thiki.kanban.teams.team.TeamsController;
-
-import javax.mail.MessagingException;
-import java.io.IOException;
 
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
@@ -16,12 +13,14 @@ import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
  * Created by xubt on 7/8/16.
  */
 public class IdentificationResource extends RestResource {
-    public IdentificationResource(Identification identification) throws TemplateException, IOException, MessagingException {
+    public IdentificationResource(Identification identification) throws Exception {
         super.domainObject = identification;
         Link boardsLink = linkTo(methodOn(BoardsController.class).loadByUserName(identification.getUserName())).withRel("boards");
         this.add(boardsLink);
 
         Link teamsLink = linkTo(methodOn(TeamsController.class).findByUserName(identification.getUserName())).withRel("teams");
         this.add(teamsLink);
+        Link notificationsLink = linkTo(methodOn(NotificationController.class).loadNotifications(identification.getUserName())).withRel("notifications");
+        this.add(notificationsLink);
     }
 }
