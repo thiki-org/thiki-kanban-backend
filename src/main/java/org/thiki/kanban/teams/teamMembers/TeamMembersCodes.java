b861bc3314cdbb52c673a9a34bb5ee2b0d1b6c46
濤 <btao.cn@gmail.com>
2016-09-24 13:57:17 +0800

    Fixing description error.

--- src/main/java/org/thiki/kanban/teams/teamMembers/TeamMembersCodes.java
+++ src/main/java/org/thiki/kanban/teams/teamMembers/TeamMembersCodes.java
@@ -6,7 +6,7 @@ import org.thiki.kanban.foundation.application.DomainOrder;
  * Created by xubt on 8/8/16.
  */
 public enum TeamMembersCodes {
-    CURRENT_USER_IS_NOT_A_MEMBER_OF_THE_TEAM("001", "当前用户非该团队成员,允许获取。");
+    CURRENT_USER_IS_NOT_A_MEMBER_OF_THE_TEAM("001", "当前用户非该团队成员。");
 
     private String code;
     private String message;
