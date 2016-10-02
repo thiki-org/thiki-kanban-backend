5ebb5fa673fbbb3a10e011f392fb98d052628b2c
濤 <btao.cn@gmail.com>
2016-10-02 17:07:53 +0800

    Refactoring code.

--- src/main/java/org/thiki/kanban/user/UsersController.java
+++ src/main/java/org/thiki/kanban/user/UsersController.java
@@ -1,8 +1,5 @@
 package org.thiki.kanban.user;
 
-import org.apache.commons.codec.binary.Base64;
-import org.apache.commons.codec.binary.StringUtils;
-import org.apache.commons.io.IOUtils;
 import org.springframework.core.io.UrlResource;
 import org.springframework.http.HttpEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,7 +8,6 @@ import org.thiki.kanban.foundation.common.Response;
 
 import javax.annotation.Resource;
 import java.io.IOException;
-import java.io.InputStream;
 
 /**
  * Created by xutao on 09/26/16.
@@ -30,8 +26,7 @@ public class UsersController {
     @RequestMapping(value = "/users/{userName}/avatar", method = RequestMethod.GET)
     public HttpEntity loadAvatar(@PathVariable("userName") String userName) throws IOException {
         UrlResource avatar = usersService.loadAvatar(userName);
-        InputStream in = avatar.getInputStream();
-        return Response.build(StringUtils.newStringUtf8(Base64.encodeBase64(IOUtils.toByteArray(in), false)));
+        return Response.build(avatar);
     }
 
     @RequestMapping(value = "/users/{userName}/profile", method = RequestMethod.GET)
