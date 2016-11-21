c55dac6c73fb9f9f9c88d22433d4925130cb7391
Tao <btao.cn@gmail.com>
2016-11-21 13:55:52 +0800

    Modify mysql_init.sql path.

--- src/main/resources/scripts/db/init_db.sh
+++ src/main/resources/scripts/db/init_db.sh
@@ -9,7 +9,11 @@ echo "init database $database_name ..."
 mysql -u "$username" "-p$password" -e  "DROP DATABASE  IF EXISTS $database_name;"
 mysql -u "$username" "-p$password" -e  "CREATE DATABASE $database_name DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;"
 mysql -u "$username" "-p$password" -e  "USE $database_name;"
-mysql -u "$username" "-p$password" "$database_name" < "mysql_init.sql"
+
+echo "current path:"
+pwd
+
+mysql -u "$username" "-p$password" "$database_name" < "src/main/resources/scripts/db/mysql_init.sql"
 
 echo "init database done."
 
