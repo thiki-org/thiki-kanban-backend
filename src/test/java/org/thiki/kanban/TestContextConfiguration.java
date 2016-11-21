29e35d015990e62dd8ea32a81673b1f4b737b77d
Tao <btao.cn@gmail.com>
2016-11-21 18:34:20 +0800

    Mock mail service.

--- src/test/java/org/thiki/kanban/TestContextConfiguration.java
+++ src/test/java/org/thiki/kanban/TestContextConfiguration.java
@@ -1,10 +1,19 @@
 package org.thiki.kanban;
 
+import freemarker.template.TemplateException;
 import org.springframework.context.annotation.Bean;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.thiki.kanban.foundation.mail.MailService;
 
+import javax.mail.MessagingException;
 import javax.sql.DataSource;
+import java.io.IOException;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.mock;
 
 /**
  * Created by xubt on 5/11/16.
@@ -16,5 +25,12 @@ public class TestContextConfiguration {
                 .setType(EmbeddedDatabaseType.HSQL)
                 .addScript("classpath:hsql.sql")
                 .build();
+    }
+
+    @Bean
+    public MailService mailService() throws TemplateException, IOException, MessagingException {
+        MailService mailService = mock(MailService.class);
+        doNothing().when(mailService).sendMailByTemplate(anyString(), anyString(), any(), anyString());
+        return mailService;
     }
 }
