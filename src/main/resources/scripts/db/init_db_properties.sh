18ede0cb3e0fd1f265bd60c6cdab91a7ee0b46de
Tao <btao.cn@gmail.com>
2016-11-21 16:07:32 +0800

    Separate  db_init into two files.

---
+++ src/main/resources/scripts/db/init_db_properties.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+set -e
+env_name=$1
+env_params_path=$2
+
+echo "init database properties ..."
+
+echo "env:$1"
+echo "env_params_path:$2"
+
+source $2
+
+if [ "$1" = "sit" ]
+then
+username=$username_sit
+password=$password_sit
+host_name=$host_name_sit
+fi
+
+if [ "$1" = "prod" ]
+then
+username=$username_prod
+password=$password_prod
+host_name=$host_name_prod
+fi
+
+echo "username: $username"
+echo "password: $password"
+echo "host_name: $host_name"
+
+echo "jdbc.url=jdbc:mysql://$host_name:3306/thiki-kanban?useUnicode=true&characterEncoding=utf8" >>src/main/resources/kanban.properties
+echo "jdbc.username=$username" >>src/main/resources/kanban.properties
+echo "jdbc.password=$password" >>src/main/resources/kanban.properties
+
+echo "init database properties done."
