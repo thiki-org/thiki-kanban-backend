117e8615cf64b43d19841d305b7e4d7acef1bede
Tao <btao.cn@gmail.com>
2016-11-22 23:05:28 +0800

    Optimize deploy script.

--- src/main/resources/scripts/deploy.sh
+++ src/main/resources/scripts/deploy.sh
@@ -2,22 +2,34 @@
 set -e
 artifacts_path=$1
 server_dir=$2
-server_addr=$3
-url=$3
 echo "artifacts_path:$1"
 echo "server_dir:$2"
-echo "server_addr:$3"
 
+echo "remove old scripts..."
+cd $server_dir
+rm -rf killServer.sh
+rm -rf deploy.sh
+
+echo "copy new scripts..."
+mv $artifacts_path/src/main/resources/scripts/deploy.sh  $server_dir
+mv $artifacts_path/src/main/resources/scripts/killServer.sh  $server_dir
+
+echo "chmod new scripts..."
+chmod 777 deploy.sh
+chmod 777 killServer.sh
+
+echo "stop server..."
 sh killServer.sh
 
-echo "clean server directory..."
-cd $server_dir
+echo "remove old jar..."
 rm -rf *.jar
 
 echo "copy new artifact to server directory..."
-mv $artifacts_path/*.jar $server_dir
+mv $artifacts_path/target/*.jar $server_dir
 
 echo "start server ..."
+cd $server_dir
+
 java -jar *.jar 1>> log.log 2>&1&
 
 echo "deploy done."
