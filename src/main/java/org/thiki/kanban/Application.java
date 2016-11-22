d6850eaf7d59ecda5e514654765e7f6847d8589c
Tao <btao.cn@gmail.com>
2016-11-22 22:40:29 +0800

    Optimize kill server logic.

--- src/main/java/org/thiki/kanban/Application.java
+++ src/main/java/org/thiki/kanban/Application.java
@@ -2,6 +2,7 @@ package org.thiki.kanban;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.system.ApplicationPidFileWriter;
 
 /**
  * Created by xubitao on 04/26/16.
@@ -9,6 +10,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) throws Exception {
-        SpringApplication.run(Application.class, args);
+        SpringApplication springApplication = new SpringApplication(Application.class);
+        springApplication.addListeners(new ApplicationPidFileWriter("thiki-kanban.pid"));
+        springApplication.run(args);
     }
 }
--- src/main/resources/scripts/deploy.sh
+++ src/main/resources/scripts/deploy.sh
@@ -7,10 +7,8 @@ url=$3
 echo "artifacts_path:$1"
 echo "server_dir:$2"
 echo "server_addr:$3"
-if curl --output /dev/null --silent --head --fail "$3"; then
-    echo "The server is running,stop server..."
-    wget --post-data ""  $server_addr
-fi
+
+sh killServer.sh
 
 echo "clean server directory..."
 cd $server_dir
