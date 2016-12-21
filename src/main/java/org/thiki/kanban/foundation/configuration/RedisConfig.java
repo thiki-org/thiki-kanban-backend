b3dce2cf8958a2ebb2c9dd0a5e106f58e4010ad0
Tao <btao.cn@gmail.com>
2016-12-21 10:58:03 +0800

    Add Profile to skip redis in junit tests.

--- src/main/java/org/thiki/kanban/foundation/configuration/RedisConfig.java
+++ src/main/java/org/thiki/kanban/foundation/configuration/RedisConfig.java
@@ -10,6 +10,7 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.interceptor.KeyGenerator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -24,6 +25,7 @@ import java.lang.reflect.Method;
 
 @Configuration
 @EnableCaching
+@Profile("!local-test")
 public class RedisConfig extends CachingConfigurerSupport {
     @Bean
     public KeyGenerator wiselyKeyGenerator() {
