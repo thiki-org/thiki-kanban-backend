6f56b618006fcb3ab17ef24ee3b51c8a62bc7d52
Tao <btao.cn@gmail.com>
2016-12-23 14:38:36 +0800

    Optimize board overall cache logic.

--- src/main/java/org/thiki/kanban/foundation/cache/CacheAspect.java
+++ src/main/java/org/thiki/kanban/foundation/cache/CacheAspect.java
@@ -5,42 +5,53 @@ import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 import org.springframework.web.util.UriTemplate;
 
+import javax.annotation.Resource;
+import java.util.Set;
+
 /**
  * Created by xubt on 10/24/16.
  */
 @Aspect
 @Component
+@ConditionalOnProperty("redis.enabled")
 public class CacheAspect {
     private static Logger logger = LoggerFactory.getLogger(CacheAspect.class);
 
-    @CacheEvict(value = "board", key = "contains('board-overall'+#result)", condition = "#result !=null ")
+    @Resource
+    private RedisTemplate<String, String> redisTemplate;
+
     @After("within(@org.springframework.web.bind.annotation.RestController *) && execution(* *(..))")
-    public String advice() {
+    public void advice() {
         ServletRequestAttributes servletRequestAttributes = (ServletRequestAttributes) RequestContextHolder.currentRequestAttributes();
         RequestFacade requestFacade = (RequestFacade) servletRequestAttributes.getRequest();
         String method = requestFacade.getMethod();
         if (HttpMethod.GET.name().equals(method)) {
-            return null;
+            return;
         }
         String userName = requestFacade.getHeader("userName");
         if (userName == null) {
-            return null;
+            return;
         }
         String uri = requestFacade.getRequestURI();
         UriTemplate template = new UriTemplate("boards/{boardId}");
         String extractedUrl = template.match(uri).get("boardId");
         if (extractedUrl == null) {
-            return null;
+            return;
         }
         String boardId = template.match(uri).get("boardId").split("/")[0];
         logger.info("board overall cache was removed.boardId:{}", boardId);
-        return boardId;
+        String keyPattern = String.format("board-overall%s*", boardId);
+        Set<byte[]> keys = redisTemplate.getConnectionFactory().getConnection().keys(keyPattern.getBytes());
+        for (byte[] key : keys) {
+            redisTemplate.delete(new String(key, 0, key.length));
+        }
     }
 }
