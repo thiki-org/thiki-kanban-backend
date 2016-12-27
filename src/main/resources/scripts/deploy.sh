bb597d563ee8e208d279a7e604bd9365127a3d77
Tao <btao.cn@gmail.com>
2016-12-27 22:45:30 +0800

    Fix deploy error.

--- src/main/resources/scripts/deploy.sh
+++ src/main/resources/scripts/deploy.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
-server_host=$1
-server_dir=$2
+artifact=$1
+server_host=$2
+server_dir=$3
 
 echo "-> upload artifact to remote server"
-scp /Users/xubt/Documents/products/thiki-kanban-backend/build/libs/kanban-1.0-SNAPSHOT.jar $server_host:$server_dir
+scp $artifact $server_host:$server_dir
 scp killServer.sh $server_host:$server_dir
 
 echo "-> login remote server"
