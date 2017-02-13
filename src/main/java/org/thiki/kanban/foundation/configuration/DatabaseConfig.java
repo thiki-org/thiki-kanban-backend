4f37a7c4ee12bdf63445bdeeae4ac62fc63f8f70
xiongshibo@chuangjia.me <xiongshibo@chuangjia.me>
2017-02-13 15:49:00 +0800

    增加druid 数据源

--- src/main/java/org/thiki/kanban/foundation/configuration/DatabaseConfig.java
+++ src/main/java/org/thiki/kanban/foundation/configuration/DatabaseConfig.java
@@ -1,21 +1,26 @@
 package org.thiki.kanban.foundation.configuration;
 
+import com.alibaba.druid.support.http.WebStatFilter;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.thiki.kanban.foundation.aspect.DBInterceptor;
+import org.springframework.boot.context.embedded.FilterRegistrationBean;
+import org.springframework.boot.context.embedded.ServletRegistrationBean;
+import com.alibaba.druid.support.http.StatViewServlet;
 
 import javax.annotation.Resource;
 import javax.sql.DataSource;
+import com.alibaba.druid.pool.DruidDataSource;
 
 @Configuration
 @ComponentScan
@@ -29,18 +34,79 @@ public class DatabaseConfig {
     private String userName;
     @Value("${jdbc.password}")
     private String password;
+    @Value("${datasource.initialSize}")
+    private int initialSize;
+    @Value("${datasource.maxActive}")
+    private int maxActive;
+    @Value("${datasource.minIdle}")
+    private int minIdle;
+    @Value("${datasource.maxWait}")
+    private int maxWait;
+    @Value("${datasource.timeBetweenEvictionRunsMillis}")
+    private long timeBetweenEvictionRunsMillis;
+    @Value("${datasource.minEvictableIdleTimeMillis}")
+    private long minEvictableIdleTimeMillis;
+    @Value("${datasource.validationQuery}")
+    private String validationQuery;
+    @Value("${datasource.testWhileIdle}")
+    private boolean testWhileIdle;
+    @Value("${datasource.testOnBorrow}")
+    private boolean testOnBorrow;
+    @Value("${datasource.testOnReturn}")
+    private boolean testOnReturn;
+    @Value("${datasource.poolPreparedStatements}")
+    private boolean poolPreparedStatements;
+    @Value("${datasource.maxPoolPreparedStatementPerConnectionSize}")
+    private int maxPoolPreparedStatementPerConnectionSize;
+    @Value("${datasource.filters}")
+    private String filters;
+	@Value("${datasource.connectionProperties}")
+    private String connectionProperties;
+    @Value("${datasource.druidUserName}")
+    private String druidUserName;
+    @Value("${datasource.druidPassword}")
+    private String druidPassword;
 
     @Resource
     private DBInterceptor dbInterceptor;
 
-    @Bean
+    @Bean(initMethod = "init", destroyMethod = "close")
+    //@ConfigurationProperties("spring.datasource.druid")
     public DataSource dataSource() {
-        DriverManagerDataSource mysql = new DriverManagerDataSource();
-        mysql.setDriverClassName(driver);
-        mysql.setUrl(url);
-        mysql.setUsername(userName);
-        mysql.setPassword(password);
-        return mysql;
+        DruidDataSource druidDataSource = new DruidDataSource();
+        druidDataSource.setUrl(this.url);
+        druidDataSource.setUsername(this.userName);
+        druidDataSource.setPassword(this.password);
+        druidDataSource.setDriverClassName(this.driver);
+        druidDataSource.setInitialSize(this.initialSize);
+        druidDataSource.setMaxActive(this.maxActive);
+        druidDataSource.setMinIdle(this.minIdle);
+        druidDataSource.setMaxWait(this.maxWait);
+        druidDataSource.setTimeBetweenEvictionRunsMillis(this.timeBetweenEvictionRunsMillis);
+        druidDataSource.setMinEvictableIdleTimeMillis(this.minEvictableIdleTimeMillis);
+        druidDataSource.setValidationQuery(this.validationQuery);
+        druidDataSource.setTestWhileIdle(this.testWhileIdle);
+        druidDataSource.setTestOnBorrow(this.testOnBorrow);
+        druidDataSource.setTestOnReturn(this.testOnReturn);
+        druidDataSource.setPoolPreparedStatements(this.poolPreparedStatements);
+        druidDataSource.setMaxPoolPreparedStatementPerConnectionSize(this.maxPoolPreparedStatementPerConnectionSize);
+        try {
+           druidDataSource.setFilters(this.filters);
+        } catch (Exception e) {
+            throw new RuntimeException("load datasource error, dbProperties is :", e);
+        }
+
+        try {
+            if(null != druidDataSource) {
+                druidDataSource.setFilters("wall,stat");
+                druidDataSource.setUseGlobalDataSourceStat(true);
+                druidDataSource.init();
+            }
+        } catch (Exception e) {
+            throw new RuntimeException("load datasource error, dbProperties is :", e);
+        }
+
+        return druidDataSource;
     }
 
     @Bean
@@ -65,5 +131,26 @@ public class DatabaseConfig {
     @Bean
     public PlatformTransactionManager transactionManager() {
         return new DataSourceTransactionManager(dataSource());
+    }
+
+
+    @Bean
+    public ServletRegistrationBean druidServlet() {
+        ServletRegistrationBean reg = new ServletRegistrationBean();
+        reg.setServlet(new StatViewServlet());
+        reg.addUrlMappings("/druid/*");
+        //reg.addInitParameter("allow", "127.0.0.1"); //白名单
+        //reg.addInitParameter("deny",""); //黑名单
+        reg.addInitParameter("loginUsername", druidUserName);
+        reg.addInitParameter("loginPassword", druidPassword);
+        return reg;
+    }
+
+    @Bean public FilterRegistrationBean filterRegistrationBean() {
+        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
+        filterRegistrationBean.setFilter(new WebStatFilter());
+        filterRegistrationBean.addUrlPatterns("/*");
+        filterRegistrationBean.addInitParameter("exclusions", "*.js,*.gif,*.jpg,*.png,*.css,*.ico,/druid/*");
+        return filterRegistrationBean;
     }
 }
