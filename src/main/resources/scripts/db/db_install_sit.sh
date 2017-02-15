0a5f40414353d8b70110698382f4ef815d860025
xwinie <xwinie@gmail.com>
2017-02-15 23:17:33 +0800

    delete used file

--- src/main/resources/scripts/db/db_install_sit.sh
+++ src/main/resources/scripts/db/db_install_sit.sh
@@ -11,9 +11,9 @@ mysql -u "$username_sit" "-p$password_sit" -e  "DROP DATABASE  IF EXISTS $databa
 mysql -u "$username_sit" "-p$password_sit" -e  "CREATE DATABASE $database_name_sit DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;"
 mysql -u "$username_sit" "-p$password_sit" -e  "USE $database_name_sit;"
 
-echo "current path:"
-pwd
+#echo "current path:"
+#pwd
 
-mysql -u "$username_sit" "-p$password_sit" "$database_name_sit" < "src/main/resources/scripts/db/mysql_init.sql"
+#mysql -u "$username_sit" "-p$password_sit" "$database_name_sit" < "src/main/resources/scripts/db/mysql_init.sql"
 
 echo "init database done."
