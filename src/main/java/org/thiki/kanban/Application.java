ca110ca526bd3a9989de32a75b3e473051f7b861
xiongshibo@chuangjia.me <xiongshibo@chuangjia.me>
2017-03-02 17:15:22 +0800

    add jmeter and actuator

--- src/main/java/org/thiki/kanban/Application.java
+++ src/main/java/org/thiki/kanban/Application.java
@@ -1,13 +1,17 @@
 package org.thiki.kanban;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.system.ApplicationPidFileWriter;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import de.codecentric.boot.admin.config.EnableAdminServer;
+
 
 /**
  * Created by xubitao on 04/26/16.
  */
+@EnableAdminServer
 @SpringBootApplication
 @EnableScheduling
 public class Application {
